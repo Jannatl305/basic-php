396d6a3e82b148361518883f4925d62801445a47
Jannatl305 <chinchanchin909@gmail.com>
2025-05-03 20:41:54 +0600

    array

---
+++ array.php
@@ -0,0 +1,44 @@
+<?php
+$practice = [
+    [
+        "class-1" => 
+        [
+            "name" => "Moni",
+            "age" => 20,
+        ]
+        ],
+
+        [
+            "class-2" => 
+            [
+                "name" => "Risha",
+                "age" => 21,
+            ]
+        ],
+
+        [
+            "class-3" => 
+            [
+                "name" => "Zihad",
+                "age" => 22,
+            ]
+        ],
+
+        [
+            "class-4" => 
+            [
+                "name" => "Jubayer",
+                "age" => 23,
+            ]
+        ]
+];
+?>
+<hr
+<table border="1" width="100%">
+    <tr>
+        <th>Class</th>
+        <th>Name</th>
+        <th>Age</th>
+        </tr>
+    <?php
+        
