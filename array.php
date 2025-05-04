b65a4782d8115e18bff5c8f5c2d9be67ee4f0aa6
Jannatl305 <chinchanchin909@gmail.com>
2025-05-04 21:33:06 +0600

    whatever

--- array.php
+++ array.php
@@ -5,40 +5,49 @@ $practice = [
         [
             "name" => "Moni",
             "age" => 20,
-        ]
         ],
-
+         "class-2" => 
         [
-            "class-2" => 
-            [
                 "name" => "Risha",
                 "age" => 21,
-            ]
         ],
-
-        [
-            "class-3" => 
-            [
+         "class-3" => 
+         [
                 "name" => "Zihad",
                 "age" => 22,
-            ]
-        ],
+            ],
+        
 
-        [
-            "class-4" => 
+             "class-4" => 
             [
                 "name" => "Jubayer",
                 "age" => 23,
-            ]
+            ],
         ]
 ];
 ?>
-<hr
-<table border="1" width="100%">
+<hr>
+<table border="1" width="30%">
     <tr>
         <th>Class</th>
         <th>Name</th>
         <th>Age</th>
+    </tr>
+     <?php foreach($practice as $class) : ?>
+     <?php foreach($class as $value => $val) : ?>
+        <tr>
+            <td><?php echo [$class] ?></td>
+            <td><?php echo $val["name"] ?></td>
+            <td><?php echo $val["age"] ?></td>
         </tr>
-    <?php
-        
+        <?php endforeach; ?> 
+        <?php endforeach; ?> 
+</table>
+
+<?php
+var_dump($class);
+echo "<br>";
+var_dump($value);
+echo "<br>";
+var_dump($val);
+?>
