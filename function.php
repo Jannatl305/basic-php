da8dfb6c750a4a1081659a73c7de437a2b16fc11
Jannatl305 <chinchanchin909@gmail.com>
2025-05-06 20:53:15 +0600

    function

---
+++ function.php
@@ -0,0 +1,22 @@
+<?php
+function cal($main_price, $dis_price) {
+    global $total_price;
+    $total_price = ($main_price * $dis_price) / 100;
+    return $total_price;
+}
+
+cal(1000, 10);
+echo $total_price;
+?>
+
+<?php
+function scop($name){
+    echo "My name is $name.";
+}
+scop("Moni");
+
+define("PI", 3.14159);
+echo PI; 
+echo "The value of PI is: " . PI . "<br>"; 
+echo "The value of PI is: " . constant("PI") . "<br>"; 
+?>
